423488ebf486559fbb8583aff5926c9417cd0655
Pragyansh Dwivedi <pragyansh99@gmail.com>
2020-11-30 02:13:40 +0530

    Firebase Signup Added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,73 @@ import React, { useState, useEffect } from "react";
 
 import './App.css';
 import Post from './Post';
-import { db } from './firebase';
+import { db, auth } from './firebase';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Input } from "@material-ui/core";
+
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
   
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        // User has logged IN 
+        console.log('User >', authUser);
+        if(authUser.displayName) {
+          // don't update their username
+        } else {
+          return authUser.updateProfile({
+            displayName:username
+          })
+        }
+      } else {
+        // User has logged OUT
+        setUser(null);
+      }
+    })
+    return () => {
+      unsubscribe();
+    }
+  }, [user,username])
 
   useEffect(() => {
     db.collection('post').onSnapshot(snapshot => {
@@ -17,15 +79,70 @@ function App() {
     })
   }, [])
 
+  const signup = (event) => {
+    event.preventDefault();
+    console.log('Signup data >>> ',event );
+    auth.createUserWithEmailAndPassword(email,password)
+    .then((authUser) => { 
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((error) => {
+      alert(error.message)
+    })
+  };
+
   return (
     <div className="App">
-      <div className="app__header">
+        
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}>
+          <div  style={modalStyle} className={classes.paper}>
+            <form className='app__signup'>
+            <center>
+            <img 
+              className="app__signupImageLogo"
+              alt="Instagram_logo"
+              src="https://play-lh.googleusercontent.com/9ASiwrVdio0I2i2Sd1UzRczyL81piJoKfKKBoC8PUm2q6565NMQwUJCuNGwH-enhm00"
+            />        
+            </center>
+            
+            <Input
+              placeholder="Username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}  
+            />
+            
+            <Input
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}  
+            />
+
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}  
+            />
+
+            <Button type="submit" onClick={signup}> Signup </Button>
+            </form>
+          </div>
+        </Modal>
+
+    <div className="app__header">
       <img 
         className="app__headerImage"
         alt="noimage"
         src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-instagram-new-circle-512.png"
       />
       </div>
+      <Button onClick={() => setOpen(true)} > Signup </Button>
 
       {
         posts.map(({ id, post}) => (<Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />))
